026a1cb12503921dd47afc5f9137487d3e8ca3ff
bjoernkarthein <bjoern.karthein@t-online.de>
2024-01-20 16:13:13 +0100

    started working on invalid value generation

--- automation/src/input_generation.py
+++ automation/src/input_generation.py
@@ -1,21 +1,53 @@
 from isla.solver import ISLaSolver
 from typing import List
 
+from mutation import ValueMutator
+
 
 class InputGenerator:
     def generate_valid_inputs(self, grammar: str, formula: str | None, amount: int = 1) -> List[str]:
+        values = []
         if amount < 1:
-            return []
+            return values
 
         solver = ISLaSolver(grammar, formula)
 
-        # try:
+        try:
+            for _ in range(amount):
+                value = str(solver.solve())
+                values.append(value)
+            return values
+        except Exception as e:
+            # TODO
+            print(e)
+            return []
+
+    def generate_invalid_inputs(self, grammar: str, formula: str | None, amount: int = 1) -> List[str]:
+        if formula is not None:
+            negated_formula = f'not ({formula})'
+            return self.generate_invalid_inputs(grammar, negated_formula, amount)
+
+        return self.__generate_invalid_values_for_non_existent_formula(grammar, amount)
+
+    def __generate_invalid_values_for_non_existent_formula(self, grammar: str, amount: int = 1) -> List[str]:
         values = []
+
         for _ in range(amount):
-            value = str(solver.solve())
-            values.append(value)
-        return values
-        # except Exception as e:
-        #     # TODO
-        #     print(e)
-        #     return []
+            found_invalid_value = False
+
+            last_value = None
+            for _ in range(100):  # TODO: think of a good number here
+                solver = ISLaSolver(grammar)
+                value = str(solver.solve())
+                # TODO: What is a good number for mutations?
+                mutator = ValueMutator(value, 5)
+                last_value = mutator.mutate(last_value)
+
+                if not solver.check(value):
+                    values.append(value)
+                    found_invalid_value = True
+                    break
+
+            if not found_invalid_value:
+                # TODO: What if I don't find something invalid
+                values.append('')
---
+++ automation/src/mutation.py
@@ -0,0 +1,46 @@
+import random
+import string
+
+
+class ValueMutator:
+    def __init__(self, input: str, mutations: int = 1) -> None:
+        self.__input = input
+        self.__mutations = mutations
+        self.__mutators = [
+            self.__delete_random_character,
+            self.__insert_random_character,
+            self.__flip_random_character
+        ]
+
+    def mutate(self, seed: str | None = None) -> str:
+        if seed is not None:
+            self.__input = seed
+
+        for _ in range(self.__mutations):
+            mutator = random.choice(self.__mutators)
+            mutator()
+
+        return self.__input
+
+    def __delete_random_character(self) -> None:
+        if len(self.__input) == 0:
+            return
+
+        pos = random.randint(0, len(self.__input) - 1)
+        self.__input = self.__input[:pos] + self.__input[pos + 1:]
+
+    def __insert_random_character(self) -> None:
+        pos = random.randint(0, len(self.__input))
+        random_character = random.choice(string.printable)
+        self.__input = self.__input[:pos] + \
+            random_character + self.__input[pos:]
+
+    def __flip_random_character(self) -> None:
+        if len(self.__input) == 0:
+            return
+
+        pos = random.randint(0, len(self.__input) - 1)
+        char = self.__input[pos]
+        bit = 1 << random.randint(0, 6)
+        new_char = chr(ord(char) ^ bit)
+        self.__input = self.__input[:pos] + new_char + self.__input[pos + 1:]
